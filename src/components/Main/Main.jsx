1791683fc616ce04399d06c79124ddc37f473cb5
Niyatizzz <niyatikolukula@gmail.com>
2024-12-15 09:46:37 +0530

    modifs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
   return (
     <div className='main'>
       <div className="nav">
-        <p>Gemini</p>
+        <p>ByteBuddy</p>
         <img src={assets.user_icon} />
       </div>
       <div className="main-container">
